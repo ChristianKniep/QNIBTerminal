e46da24e9bca723b9828e3d4e575ba36fef78807
Christian Kniep <christian@qnib.org>
2016-02-09 13:17:14 +0100

    split out debian targets

--- docker_alpine.mk
+++ docker_alpine.mk
@@ -1,20 +1,29 @@
+.PHONY: alpn-base alpn-openrc alpn-consul alpn-syslog alpn-terminal alpn-jre8 alpn-jdk8
+
 alpn-base:
+	$(QNIB_CHECKOUT)
 	$(PLAIN_BUILD)
 
 alpn-openrc: alpn-base
+	$(QNIB_CHECKOUT)
 	$(PLAIN_BUILD)
 
 alpn-consul: alpn-openrc
+	$(QNIB_CHECKOUT)
 	$(PLAIN_BUILD)
 
 alpn-syslog: alpn-consul
+	$(QNIB_CHECKOUT)
 	$(PLAIN_BUILD)
 
 alpn-terminal: alpn-syslog
+	$(QNIB_CHECKOUT)
 	$(PLAIN_BUILD)
 
-alpn-jre8: alpn-terminal
-	$(PLAIN_BUILD)
+#alpn-jre8: alpn-terminal
+#	$(QNIB_CHECKOUT)
+#	$(PLAIN_BUILD)
 
 alpn-jdk8: alpn-terminal
+	$(QNIB_CHECKOUT)
 	$(PLAIN_BUILD)
---
+++ docker_debian.mk
@@ -0,0 +1,30 @@
+.PHONY: d-hadoop d-java7 d-terminal d-supervisor d-syslog d-consul d-terminal d-node rocketchat hubot-rocketchat
+
+d-supervisor:
+	$(PLAIN_BUILD)
+
+d-syslog: d-supervisor
+	$(PLAIN_BUILD)
+
+d-consul: d-syslog
+	$(PLAIN_BUILD)
+
+d-terminal: d-consul
+	$(PLAIN_BUILD)
+
+d-node: d-terminal
+	$(PLAIN_BUILD)
+
+rocketchat: d-node
+	cd ~/docker/docker-$@; $(MAKE)
+
+hubot-rocketchat: d-node
+	cd ~/docker/docker-$@; $(MAKE)
+
+d-java7: d-terminal
+	$(QNIB_CHECKOUT)
+	$(PLAIN_BUILD)
+
+d-hadoop: d-java7
+	$(QNIB_CHECKOUT)
+	$(PLAIN_BUILD)
