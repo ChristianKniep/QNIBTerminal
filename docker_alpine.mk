6dd952e11065d7a1c4fa52d2383c9e9559321396
Christian Kniep <christian@qnib.org>
2016-02-12 21:36:00 +0100

    add gocd

--- docker_alpine.mk
+++ docker_alpine.mk
@@ -4,7 +4,10 @@ alpn-base:
 alpn-openrc: alpn-base
 	$(PLAIN_BUILD)
 
-alpn-consul: alpn-openrc
+alpn-supervisor: alpn-base
+	$(PLAIN_BUILD)
+
+alpn-consul: alpn-supervisor
 	$(PLAIN_BUILD)
 
 alpn-syslog: alpn-consul
@@ -16,5 +19,14 @@ alpn-terminal: alpn-syslog
 alpn-jre8: alpn-terminal
 	$(PLAIN_BUILD)
 
+alpn-jre7: alpn-syslog
+	$(PLAIN_BUILD)
+
 alpn-jdk8: alpn-terminal
+	$(PLAIN_BUILD)
+
+alpn-gocd-agent: alpn-jre7
+	$(PLAIN_BUILD)
+
+docker-gocd-server: alpn-jre7
 	$(PLAIN_BUILD)
